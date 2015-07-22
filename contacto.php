c03604126a3130214b04909b976d3fd5d854eb17
Victor <mastershack@gmail.com>
2015-07-22 21:00:08 +0000

    Ampliando comentarios prueba_2

--- contacto.php
+++ contacto.php
@@ -20,7 +20,7 @@
 
 
 </script>
-<!--[if lt IE 8]>
+<!--[if lt InternetExplorer 8]>
       <div style=' clear: both; text-align:center; position: relative;'>
         <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
           <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
